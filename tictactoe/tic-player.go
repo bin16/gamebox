2544b6e2f8815d66dbe3c2bd0e5b9b6b1a306788
bin16 <bin16@outlook.com>
2020-08-16 15:09:20 +0800

    TicTacToe edit, and tests

--- tictactoe/tic-player.go
+++ tictactoe/tic-player.go
@@ -1,5 +1,9 @@
 package tictactoe
 
+import (
+	"fmt"
+)
+
 func (g *Game) playerInGame(playerID string) bool {
 	for _, p := range g.Players {
 		if p == playerID {
@@ -8,6 +12,78 @@ func (g *Game) playerInGame(playerID string) bool {
 	}
 
 	return false
+}
+
+func (g *Game) winnerSide() Side {
+	flat4int := func(d [4]int) (int, int, int, int) {
+		return d[0], d[1], d[2], d[3]
+	}
+	dl := [][4]int{
+		[4]int{0, 0, 1, 0},
+		[4]int{0, 0, 1, 1},
+		[4]int{0, 0, 0, 1},
+		[4]int{0, 1, 1, 0},
+		[4]int{0, 2, 1, 0},
+		[4]int{0, 2, 1, -1},
+	}
+	for _, d := range dl {
+		x0, y0, dx, dy := flat4int(d)
+		c0 := g.Board[x0][y0]
+		l := g.lineOf(x0, y0, dx, dy, boardSize)
+		for i, c := range l {
+			if c != c0 {
+				break
+			}
+			if c == c0 && i == len(l)-1 {
+				return c0
+			}
+		}
+	}
+	fmt.Println("XXXXXXX", "end", dl, g.Board)
+
+	return Blank
+}
+
+func (g *Game) lineOf(x0, y0, dx, dy, max int) []Side {
+	l := []Side{}
+	for i := 0; i < max; i++ {
+		x := x0 + dx*i
+		y := y0 + dy*i
+		if len(l) == max || x == boardSize || y == boardSize {
+			return l
+		}
+		c := g.Board[x][y]
+		l = append(l, c)
+	}
+
+	return l
+}
+
+func (g *Game) isWin(x, y int) bool {
+	dl := [][4]int{
+		[4]int{0, y, 1, 0}, // right
+		[4]int{x, 0, 0, 1}, // bottom
+	}
+	if y == x {
+		dl = append(dl, [4]int{0, 0, 1, 1}) // bottomRight
+	} else if x+y == boardSize-1 {
+		dl = append(dl, [4]int{0, 2, 1, -1}) // topRight
+	}
+
+	c0 := g.Board[x][y]
+	for _, p := range dl {
+		l := g.lineOf(p[0], p[1], p[2], p[3], boardSize)
+		for i, c := range l {
+			if c != c0 {
+				return false
+			}
+			if c == c0 && i == len(l)-1 {
+				return true
+			}
+		}
+	}
+
+	return true
 }
 
 func (g *Game) Play(playerID, cmd string) error {
@@ -28,9 +104,8 @@ func (g *Game) Play(playerID, cmd string) error {
 	g.Board[x][y] = s
 	g.History = append(g.History, [3]int{int(s), x, y})
 
-	if len(g.History) == boardSize*boardSize {
+	if len(g.History) == boardSize*boardSize || g.isWin(x, y) {
 		g.Status = StatusEnded
-		return g.errorOf(GameEnd) // TODO: check here
 	}
 
 	return nil
@@ -52,4 +127,19 @@ func (g *Game) Join(playerID string) error {
 	}
 
 	return nil
+}
+
+func (g *Game) End() (bool, Side) {
+	return g.Status == StatusEnded, g.winnerSide()
+}
+
+func (g *Game) Info(playerID string) map[string]interface{} {
+	return map[string]interface{}{
+		"board":  g.Board,
+		"status": g.Status,
+		"player": map[string]interface{}{
+			"id":   playerID,
+			"open": g.nextSide() == g.sideOf(playerID),
+		},
+	}
 }
--- tictactoe/tic_test.go
+++ tictactoe/tic_test.go
@@ -5,14 +5,95 @@ import (
 	"testing"
 )
 
+func TestWinCheck(t *testing.T) {
+	p1 := "Foo"
+	p2 := "Bar"
+	g := NewGame()
+	mustJoin(t, g)(p1)
+	mustJoin(t, g)(p2)
+
+	winnerIs(t, g)(Blank)
+
+	g.Board = [boardSize][boardSize]Side{
+		[boardSize]Side{X, O, Blank},
+		[boardSize]Side{X, O, Blank},
+		[boardSize]Side{X, Blank, Blank},
+	}
+	winnerIs(t, g)(X)
+	lineIsSides(t, g)(0, 0, 1, 0, [boardSize]Side{X, X, X})
+	lineIsSides(t, g)(0, 0, 1, 1, [boardSize]Side{X, O, Blank})
+	lineIsSides(t, g)(0, 0, 0, 1, [boardSize]Side{X, O, Blank})
+	pieceIsWin(t, g)(0, 0, true)
+
+	lineIsSides(t, g)(1, 0, 0, 1, [boardSize]Side{X, O, Blank})
+	pieceIsWin(t, g)(1, 0, true)
+
+	lineIsSides(t, g)(2, 0, 0, 1, [boardSize]Side{X, Blank, Blank})
+	pieceIsWin(t, g)(2, 0, true)
+
+	pieceIsWin(t, g)(0, 1, false)
+	pieceIsWin(t, g)(1, 1, false)
+	pieceIsWin(t, g)(2, 1, false)
+
+	g.Board = [boardSize][boardSize]Side{
+		[boardSize]Side{X, O, Blank},
+		[boardSize]Side{O, X, Blank},
+		[boardSize]Side{X, Blank, X},
+	}
+	winnerIs(t, g)(X)
+
+	g.Board = [boardSize][boardSize]Side{
+		[boardSize]Side{X, O, O},
+		[boardSize]Side{O, O, Blank},
+		[boardSize]Side{O, Blank, X},
+	}
+	winnerIs(t, g)(O)
+}
+
 func TestTicTacToe(t *testing.T) {
 	p1 := "Foo"
 	p2 := "Bar"
 	g := NewGame()
 	mustJoin(t, g)(p1)
 	mustJoin(t, g)(p2)
-	mustPlay(t, g)(p1, "b2")
-	mustPlay(t, g)(p2, "c3")
+	gameIs(t, g)(StatusStarted)
+	mustPlay(t, g)(p1, "b2") // 1
+	gameIs(t, g)(StatusStarted)
+	winnerIs(t, g)(Blank)
+	mustPlay(t, g)(p2, "c3") // 2
+	mustPlay(t, g)(p1, "c2") // 3
+	mustPlay(t, g)(p2, "a2") // 4
+	mustPlay(t, g)(p1, "a3") // 5
+	mustPlay(t, g)(p2, "b1") // 6
+	mustPlay(t, g)(p1, "a1") // 7
+	winnerIs(t, g)(Blank)
+	mustPlay(t, g)(p2, "c1") // 8
+	gameIsEnd(t, g)(false, Blank)
+	winnerIs(t, g)(Blank)
+	mustPlay(t, g)(p1, "b3") // 9
+	gameIsEnd(t, g)(true, Blank)
+	gameIs(t, g)(StatusEnded)
+	winnerIs(t, g)(Blank)
+
+	g2 := NewGame()
+	gameIs(t, g2)(StatusOpen)
+	mustJoin(t, g2)(p1)
+	gameIs(t, g2)(StatusOpen)
+	mustJoin(t, g2)(p2)
+	gameIs(t, g2)(StatusStarted)
+	mustPlay(t, g2)(p1, "b2") // 1
+	gameIs(t, g2)(StatusStarted)
+	winnerIs(t, g2)(Blank)
+	mustPlay(t, g2)(p2, "c3") // 2
+	winnerIs(t, g2)(Blank)
+	mustPlay(t, g2)(p1, "c2") // 3
+	winnerIs(t, g2)(Blank)
+	mustPlay(t, g2)(p2, "b3") // 4
+	winnerIs(t, g2)(Blank)
+	mustPlay(t, g2)(p1, "a2") // 5
+	gameIsEnd(t, g2)(true, X)
+	gameIs(t, g2)(StatusEnded)
+	winnerIs(t, g2)(X)
 }
 
 func TestTools(t *testing.T) {
@@ -66,12 +147,12 @@ func (g *Game) print() {
 	for i, log := range g.History {
 		x := log[1]
 		y := log[2]
-		logText += fmt.Sprintf("0%d. %s - %s", i+1, piece(Side(log[0])), g.nameOf(x, y))
+		logText += fmt.Sprintf("0%d. %s - %s\n", i+1, piece(Side(log[0])), g.nameOf(x, y))
 	}
 	fmt.Printf(`====
 Status: %s
 Players: %v
-Next: %s (%d)
+Next: %s (Side=<%d>)
 ----
 %s----
 %s
@@ -86,6 +167,7 @@ func piece(c Side) string {
 
 func mustPlay(t *testing.T, g *Game) func(string, string) {
 	return func(playerID, cmd string) {
+		t.Helper()
 		if err := g.Play(playerID, cmd); err != nil {
 			t.Errorf("Failed to run g.Play(%s, %s), got error <%v>", playerID, cmd, err)
 			g.print()
@@ -95,9 +177,63 @@ func mustPlay(t *testing.T, g *Game) func(string, string) {
 
 func mustJoin(t *testing.T, g *Game) func(string) {
 	return func(playerID string) {
+		t.Helper()
 		if err := g.Join(playerID); err != nil {
 			t.Errorf("Failed to run g.Join(%s), got error <%v>", playerID, err)
 			g.print()
+		}
+	}
+}
+
+func gameIs(t *testing.T, g *Game) func(Status) {
+	return func(s Status) {
+		t.Helper()
+		if g.Status != s {
+			t.Errorf("Failed to check g.Status, got %d, want %d", g.Status, s)
+			g.print()
+		}
+	}
+}
+
+func winnerIs(t *testing.T, g *Game) func(Side) {
+	return func(c Side) {
+		t.Helper()
+		if g.winnerSide() != c {
+			t.Errorf("Failed to run g.winnerSide(), got %d, want %d", g.winnerSide(), c)
+			g.print()
+		}
+	}
+}
+
+func lineIsSides(t *testing.T, g *Game) func(int, int, int, int, [boardSize]Side) {
+	return func(x0, y0, dx, dy int, l [boardSize]Side) {
+		t.Helper()
+		l1 := g.lineOf(x0, y0, dx, dy, boardSize)
+		for i, p := range l1 {
+			if p != l[i] {
+				t.Errorf("Failed to run g.lineOf(%d, %d, %d, %d, %d); want %v; got %v", x0, y0, dx, dy, boardSize, l, l1)
+			}
+		}
+	}
+}
+
+func pieceIsWin(t *testing.T, g *Game) func(int, int, bool) {
+	return func(x, y int, b bool) {
+		t.Helper()
+		if g.isWin(x, y) != b {
+			h := g.lineOf(0, y, 1, 0, boardSize)
+			v := g.lineOf(x, 0, 0, 1, boardSize)
+			t.Errorf("Failed to run g.isWin(%d, %d), got %v, want %v; \nH: %v;\nV: %v;\nBoard:%v", x, y, g.isWin(x, y), b, h, v, g.Board)
+			g.print()
+		}
+	}
+}
+
+func gameIsEnd(t *testing.T, g *Game) func(bool, Side) {
+	return func(e bool, c Side) {
+		t.Helper()
+		if e1, c1 := g.End(); e1 != e || c1 != c {
+			t.Errorf("Failed to run g.End(); got %v,%v; want %v,%v", e1, c1, e, c)
 		}
 	}
 }
